824663ed152f984ada13b1f7d1cf699852d582e9
Dev Parapalli <devparapalli@gmail.com>
2021-11-10 17:01:55 +0530

    Added the initial files

---
+++ svelte.config.js
@@ -0,0 +1,26 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		ssr: false,
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		})
+	}
+};
+
+export default config;
